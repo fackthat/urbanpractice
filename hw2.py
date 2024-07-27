a3f2763e399852c8c5f806de545263f2be1eb991
Kirill Boyko <boykoke@gmail.com>
2024-07-27 13:23:51 +0300

    Add files via upload
    homework2

---
+++ hw2.py
@@ -0,0 +1,5 @@
+done_homework_quantity = 12
+hours_used = 1.5
+course_name = "Python"
+time_per_job = (hours_used / done_homework_quantity)
+print("Курс: ", course_name, ", всего задач:", done_homework_quantity, ", затрачено часов: ", hours_used, ", среднее время выполнения ", time_per_job, " часа.", sep='')

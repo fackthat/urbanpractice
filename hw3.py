9352b6e26c1d97873382197eb6f35d92532e5ace
Kirill Boyko <boykoke@gmail.com>
2024-07-27 13:47:51 +0300

    Add files via upload
    homework3

---
+++ hw3.py
@@ -0,0 +1,8 @@
+name = 'Kirill'
+print("Name: ", name, sep='')
+age = 33
+print("Age: ", age, sep='')
+age = age + 7
+print("New Age: ", age, sep='')
+is_student = True
+print("Is Student: ", is_student, sep='')

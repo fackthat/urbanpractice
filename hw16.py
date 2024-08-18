86b577ef840a0c23934dcf359f582a7201382e34
Kirill Boyko <boykoke@gmail.com>
2024-08-18 14:44:56 +0300

    Update hw16.py
    homeword16_update

--- hw16.py
+++ hw16.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if i.lower() in root_word or root_word.lower() in i.lower():
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
             same_words.append(i)
     return same_words
 

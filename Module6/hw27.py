6447efb0cf09ddafa311b2c6886f8c2bfe9e45e7
Kirill Boyko <boykoke@gmail.com>
2024-09-16 22:27:27 +0300

    Add files via upload
    homework27

---
+++ Module6/hw27.py
@@ -0,0 +1,60 @@
+
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+        self.__is_uppercase = False
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        if self.__is_uppercase:
+            return f'Цвет: {self.__color.upper()}'
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f'Владелец {self.owner}')
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in Vehicle.__COLOR_VARIANTS:
+            self.__color == new_color.lower()
+            self.__is_uppercase = True
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+        super().__init__(owner, model, color, engine_power)
+
+    def get_passengers_limit(self):
+        return f"Лимит пассажиров: {Sedan.__PASSENGERS_LIMIT}"
+
+
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()

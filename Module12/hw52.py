3ea3b12dade82789a2487f68a9ec7e7399ced9a5
Kirill Boyko <boykoke@gmail.com>
2024-11-16 21:19:46 +0300

    Add files via upload
    homework54
    added decorator skip_if_frozen

--- Module12/hw52.py
+++ Module12/hw52.py
@@ -1,19 +1,31 @@
 from runner import Runner
 import unittest
 
+def skip_if_frozen(method):
+    def wrapper(self, *args, **kwargs):
+        if self.is_frozen:
+            self.skipTest("Тесты в этом кейсе заморожены")
+        return method(self, *args, **kwargs)
+    return wrapper
+
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @skip_if_frozen
     def test_walk(self):
         runner = Runner('TestRunner')
         for i in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50, 'Distance after 10 walks should be 50')
 
+    @skip_if_frozen
     def test_run(self):
         runner = Runner('TestRunner')
         for i in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100, 'Distance after 10 walks should be 100')
 
+    @skip_if_frozen
     def test_challenge(self):
         runner1 = Runner('TestRunner1')
         runner2 = Runner('TestRunner2')

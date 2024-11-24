21c09c2e81d345f2a2af8817bb36e08f619a51b0
Kirill Boyko <boykoke@gmail.com>
2024-11-24 20:38:44 +0300

    Create Bot_v2.py

---
+++ Module13/Bot_v2.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '7520610123:AAGG4iJxxRXRHoWWG8pNdFMNLWuYFhk_7KM'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+
+@dp.message_handler(text = ['Urban'])
+async def urban_message(message):
+    print('Urban message')
+
+# @dp.message_handler(commands=['start'])
+# async def start_message(message):
+#     print('start message')
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print(f'Привет! Я бот, помогающий твоему здоровью')
+    await message.answer('Привет! Я бот, помогающий твоему здоровью')
+
+# @dp.message_handler()
+# async def all_message(message):
+#     print('Мы получили сообщение!')
+
+@dp.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение')
+    await message.answer('Введите команду /start, чтобы начать общение')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)

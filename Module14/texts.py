ceb7cb91705a1113f3b39e37a02d5b88c8c3e121
Kirill Boyko <boykoke@gmail.com>
2024-12-08 00:15:29 +0300

    Add files via upload
    texts_hw58

---
+++ Module14/texts.py
@@ -0,0 +1,16 @@
+from config import *
+
+formula_text = (f'Формула Миффлина-Сан Жеора\n\n' 'Для мужчин: калории = 10 * вес (кг) + 6.25 * рост (см) - 5 * возраст (лет) + 5\n\n' 'Для женщин вместо +5 используйте -161')
+
+welcome = f'Привет! Я бот, помогающий твоему здоровью. Введи слово "Calories", чтобы узнай свою норму потребления калорий в день'
+
+info = f'Этот бот помогает рассчитать норму калорий на основе ваших параметров.'
+
+choose = f'Выберите действие: '
+
+products = [
+        {"name": "Vitamin A", "desc": "Витамин Д", "price": 100, "image_path": "files/Vitamin_A.jpg"},
+        {"name": "Vitamin C", "desc": "Витамин Ц", "price": 200, "image_path": "files/Vitamin_C.jpg"},
+        {"name": "Vitamin D", "desc": "Витамин Б", "price": 300, "image_path": "files/Vitamin_D.jpg"},
+        {"name": "Vitamin E", "desc": "Витамин Е", "price": 400, "image_path": "files/Vitamin_E.jpg"}
+    ]
